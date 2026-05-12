c0dbecf6b3ea92fddb43f4d2f176b709f65ed160
mestregeorge <georgeteixeiraaguiar@gmail.com>
2026-05-12 16:43:57 +0100

    added fix date

--- frontend/src/lib/launch.js
+++ frontend/src/lib/launch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const LAUNCH_DATE = new Date(2026, 4, 16);
+export const LAUNCH_DATE = new Date(2026, 2, 16);
 export const LAUNCH_MESSAGE = "*Launching May 16, 2026 - Sign up now. The more users, the better your matches later.";
 export const SEARCH_LOCKED_MESSAGE = "*This feature will only be available on 16 May 2026";
 
